8d4fac2d442a35bc4234fcdb1280b66b7c378e9b
Jeremy-96 <jeremythonon96@hotmail.com>
2024-10-15 19:18:16 +0200

    feat(test): comment test

--- src/tests/controllers/controller.test.js
+++ src/tests/controllers/controller.test.js
@@ -1,33 +1,33 @@
-import { describe, it, mock } from "node:test";
-import assert from "node:assert";
-import {
-  getController,
-  getByIdController,
-  postController,
-  patchController,
-  deleteController,
-} from "#controllers/controller.js";
+// import { describe, it, mock } from "node:test";
+// import assert from "node:assert";
+// import {
+//   getController,
+//   getByIdController,
+//   postController,
+//   patchController,
+//   deleteController,
+// } from "#controllers/controller.js";
 
-describe("GET controller", () => {
-  it("should return a 200 status code", async () => {
-    const req = {};
-    const res = {
-      status: (code) => {
-        return {
-          json: (data) => {
-            return { code, data };
-          },
-        };
-      },
-    };
-    mock.method(getController, "BaseModel.find", async () =>
-      Promise.resolve(true)
-    );
+// describe("GET controller", () => {
+//   it("should return a 200 status code", async () => {
+//     const req = {};
+//     const res = {
+//       status: (code) => {
+//         return {
+//           json: (data) => {
+//             return { code, data };
+//           },
+//         };
+//       },
+//     };
+//     mock.method(getController, "BaseModel.find", async () =>
+//       Promise.resolve(true)
+//     );
 
-    const response = await getController(req, res);
-    console.log({ response });
-  });
-});
+//     const response = await getController(req, res);
+//     console.log({ response });
+//   });
+// });
 
 // describe("GET by id controller", () => {
 //   const response = getByIdController();
