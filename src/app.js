74d824494d12da48603cf565052665c1b3ba0293
Jeremy-96 <jeremythonon96@hotmail.com>
2024-11-20 19:08:13 +0100

    feat(limiter): add a request limiter to manage number of request

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { rateLimit } from 'express-rate-limit';
 import router from '#routes/router.js';
-import { databaseConnection } from '#utils/database.js';
 import { errorHandler } from '#controllers/errorController.js';
+import { databaseConnection } from '#utils/database.js';
 import AppError from '#utils/appError.js';
 
 dotenv.config();
@@ -14,11 +15,16 @@ const uri = process.env.DATABASE_URI.replace(
   process.env.DATABASE_PASSWORD,
 );
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request',
+});
 databaseConnection(uri);
 
 app.use(cors());
 app.use(express.json());
-app.use(process.env.API_BASE_PATH, router);
+app.use(process.env.API_BASE_PATH, limiter, router);
 app.all('*', (req, res, next) => {
   next(new AppError(`Cannot find ${req.originalUrl}`, 404));
 });
