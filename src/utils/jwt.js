fced6284d4dff6ff9c36fb0e8a4ca133cde17e4b
Jeremy-96 <jeremythonon96@hotmail.com>
2024-11-20 18:50:33 +0100

    feat(auth): send a cookie containing the jwt

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -6,8 +6,18 @@ function signToken(id) {
   });
 }
 
-const createSendToken = (user, statusCode, res) => {
+function createSendToken(user, statusCode, res) {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
 
   res.status(statusCode).json({
     status: 'success',
@@ -16,6 +26,6 @@ const createSendToken = (user, statusCode, res) => {
       user,
     },
   });
-};
+}
 
 export default createSendToken;
