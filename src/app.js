0f89a23c7269fd1d42db86ac01139cc725dc17a8
Jeremy-96 <jeremythonon96@hotmail.com>
2024-11-22 18:24:44 +0100

    feat(security): add some security packages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean'
 import { rateLimit } from 'express-rate-limit';
 import router from '#routes/router.js';
 import { errorHandler } from '#controllers/errorController.js';
@@ -20,8 +23,11 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many request',
 });
+
 databaseConnection(uri);
 
+app.use(helmet());
+app.use(ExpressMongoSanitize());
 app.use(cors());
 app.use(express.json());
 app.use(process.env.API_BASE_PATH, limiter, router);
